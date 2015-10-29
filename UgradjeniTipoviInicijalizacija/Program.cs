b6d64bcb2aa09104ec0b1d749a5138e126fbb5bd
josipjuretic <juretic.josip@gmail.com>
2015-10-29 21:18:35 +0100

    Inicijalizacija glavnih tipova

--- UgradjeniTipoviInicijalizacija/Program.cs
+++ UgradjeniTipoviInicijalizacija/Program.cs
@@ -12,11 +12,9 @@ namespace CSharp.TipoviOperatori
             // ugrađeni tipovi se mogu inicijalizirati samo operatorom
             // pridruživanja (=) ili podrazumijevanim konstruktorom!
             int i0 = new int();
-            int i1 = new int(1);
             int i2 = 2;
 
-            string s0 = new string();
-            string s1 = new string("string 1");
+           
             string s2 = new string(new char[]{'s', 't', 'r', 'i', 'n', 'g', ' ', '2'});
             string s3 = "string 3";
         }
